ade6cbb865ce2a226c216bffdd7f02f25ea483f3
Joseph Merkel <joseph_merkel@icloud.com>
2019-10-13 15:34:18 -0700

    Adding Election Audit code

--- PyPoll.py
+++ PyPoll.py
@@ -19,6 +19,13 @@ total_votes = 0
 #Candidate Options
 candidate_options = []
 
+#Candidate Tally
+candidate_votes = {}
+
+#Winning Candidate and Winning Count Tracker
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
 
 #Open the results & Read the file
 with open(file_to_load) as election_data:
@@ -32,10 +39,26 @@ with open(file_to_load) as election_data:
         candidate_name = row[2]
         if candidate_name not in candidate_options:
             candidate_options.append(candidate_name)
-        
-        
-print(candidate_options)
+            candidate_votes[candidate_name] = 0
+        candidate_votes[candidate_name] += 1
 
+for candidate in candidate_votes:
+    votes = candidate_votes[candidate]  
+    vote_percentage = (votes/total_votes) * 100
+    print(f"{candidate}: {vote_percentage:.2f}% ({votes:,})\n")
+
+    if (votes > winning_count) and (vote_percentage > winning_percentage):
+        winning_count = votes
+        winning_percentage = vote_percentage
+        winning_candidate = candidate
+    
+    winning_candidate_summary = (
+        f"--------------------------\n"
+        f"Winner: {winning_candidate}\n"
+        f"Winning Vote Count: {winning_count:,}\n"
+        f"Winning Percentage: {winning_percentage:.2f}%\n"
+        f"--------------------------\n")
+    print(winning_candidate_summary)
 
 
 #using the open funtion with the "w" mode we will write data to the file
